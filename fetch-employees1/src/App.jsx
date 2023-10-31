f2f0ffbb1449e118cd2f61918c4f13c7205fe4ca
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-31 11:44:52 +0100

    Update App.jsx

--- fetch-employees1/src/App.jsx
+++ fetch-employees1/src/App.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 import "./App.css";
+import DisplayEmployee from "./components/DisplayEmployee";
 
 function App() {
+  const sampleEmployee = {
+    gender: "male",
+    name: {
+      first: "Charlie",
+      last: "Thompson",
+    },
+    location: {
+      street: {
+        number: 761,
+        name: "Tay Street",
+      },
+      city: "Timaru",
+      postcode: 76111,
+    },
+    email: "charlie.thompson@example.com",
+    picture: {
+      medium: "https://randomuser.me/api/portraits/med/men/40.jpg",
+    },
+  };
   return (
     <>
-      <div>frederic</div>
+      <DisplayEmployee employee={sampleEmployee} />
     </>
   );
 }

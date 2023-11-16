c153f025b397b2ba543008ed57d529729f321511
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-16 11:25:33 +0100

    Update App.jsx

--- fetch-employees1/src/App.jsx
+++ fetch-employees1/src/App.jsx
@@ -1,20 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import "./App.css";
-import DisplayEmployee from "src/components/DisplayEmployee";
 import axios from "axios";
+import DisplayEmployee from "./components/DisplayEmployee";
 function App() {
-  const [employee, setEmployee] = useState(employee);
+  const [employee, setEmployee] = useState();
   const getEmployee = () => {
     // Send the request
     axios
       .get("https://randomuser.me/api?nat=en")
       // Use this data to update the state
       .then((response) => {
+        console.log("response fetch", response);
         setEmployee(response.data.results[0]);
       });
   };
-
+  console.log("state", employee);
   return (
     <div className="App">
       <DisplayEmployee employee={employee} />
